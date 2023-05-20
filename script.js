1bee29924ca3e0e6aa2dc6d2b7fbc7524b31d08c
Nathan <pythoniszen@gmail.com>
2023-05-19 17:35:59 -0700

    5/19/23 Add more sounds as well as a JS function to stop a bass sound when another one is playing

--- script.js
+++ script.js
@@ -4,8 +4,37 @@ window.addEventListener('keydown', function(e) {
   const key = document.querySelector(`.key[data-key="${e.code}"]`);
   if (!audio) return;
   audio.currentTime = 0;
+
+  if (e.code === 'KeyJ') {
+    const keyKAudio = document.querySelector(`audio[data-key="KeyK"]`);
+    const keyLAudio = document.querySelector(`audio[data-key="KeyL"]`);
+
+    if (isPlaying(keyKAudio) || isPlaying(keyLAudio)) {
+      keyKAudio.pause();
+      keyLAudio.pause();
+    }
+  } else if (e.code === 'KeyK') {
+    const keyJAudio = document.querySelector(`audio[data-key="KeyJ"]`);
+    const keyLAudio = document.querySelector(`audio[data-key="KeyL"]`);
+
+    if (isPlaying(keyJAudio) || isPlaying(keyLAudio)) {
+      keyJAudio.pause();
+      keyLAudio.pause();
+    }
+  } else if (e.code === 'KeyL') {
+    const keyJAudio = document.querySelector(`audio[data-key="KeyJ"]`);
+    const keyKAudio = document.querySelector(`audio[data-key="KeyK"]`);
+
+    if (isPlaying(keyJAudio) || isPlaying(keyKAudio)) {
+      keyJAudio.pause();
+      keyKAudio.pause();
+    }
+  }
+
   audio.play();
   key.classList.add('playing');
+
+  
 });
 
 window.addEventListener('keyup', function(e) {
@@ -30,3 +59,12 @@ keys.forEach(function(key) {
 
   });
 });
+
+
+function isPlaying(audio) {
+  if (audio && !audio.paused) {
+    return true;
+  } else {
+    return false;
+  }
+}
