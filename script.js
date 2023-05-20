2ab02cc65e4e60e335bd4f6b807b7f5780cd81f2
Nathan <pythoniszen@gmail.com>
2023-05-19 20:20:12 -0700

    5/19/23 Add click functionality for every key and refactor JavaScript code into new functions

--- script.js
+++ script.js
@@ -5,19 +5,20 @@ window.addEventListener('keydown', function(e) {
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-  key.classList.add('playing');
-
-  window.addEventListener('keyup', function(e) {
-    audio.pause();
-    const key = document.querySelector(`.key[data-key = "${e.code}"]`);
-    key.classList.remove('playing');
-  });
+  key.classList.toggle('playing');
   
 });
 
-//Plays the audio which corresponds to the div clicked
-const keys = document.querySelectorAll('.key');
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
 
+//Removes the class playing when the transition ends
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+//Plays the audio which corresponds to the div clicked
 keys.forEach(function(key) {
   key.addEventListener('click', function(e) {
 
@@ -26,18 +27,61 @@ keys.forEach(function(key) {
 
     if (clickedDivClass === 'snare') {
       const audio = document.querySelector(`audio[data-key="KeyA"]`);
-      audio.currentTime = 0;
-      audio.play();
+      const key = document.querySelector(`.key[data-key="KeyA"]`);
+      playAudio(audio, key);
+    } else if (clickedDivClass === 'hihat') {
+      const audio = document.querySelector(`audio[data-key="KeyS"]`);
+      const key = document.querySelector(`.key[data-key="KeyS"]`);
+      playAudio(audio, key);
+    } else if (clickedDivClass === 'kick') {
+      const audio = document.querySelector(`audio[data-key="KeyD"]`);
+      const key = document.querySelector(`.key[data-key="KeyD"]`);
+      playAudio(audio, key);
+    } else if (clickedDivClass === 'openhat') {
+      const audio = document.querySelector(`audio[data-key="KeyF"]`);
+      const key = document.querySelector(`.key[data-key="KeyF"]`);
+      playAudio(audio, key);
+    } else if (clickedDivClass === 'perc') {
+      const audio = document.querySelector(`audio[data-key="KeyG"]`);
+      const key = document.querySelector(`.key[data-key="KeyG"]`);
+      playAudio(audio, key);
+    } else if (clickedDivClass === 'tom') {
+      const audio = document.querySelector(`audio[data-key="KeyH"]`);
+      const key = document.querySelector(`.key[data-key="KeyH"]`);
+      playAudio(audio, key);
+    } else if (clickedDivClass === 'shaker') {
+      const audio = document.querySelector(`audio[data-key="KeyJ"]`);
+      const key = document.querySelector(`.key[data-key="KeyJ"]`);
+      playAudio(audio, key);
+    } else if (clickedDivClass === 'perc2') {
+      const audio = document.querySelector(`audio[data-key="KeyK"]`);
+      const key = document.querySelector(`.key[data-key="KeyK"]`);
+      playAudio(audio, key);
+    } else if (clickedDivClass === 'perc3') {
+      const audio = document.querySelector(`audio[data-key="KeyL"]`);
+      const key = document.querySelector(`.key[data-key="KeyL"]`);
+      playAudio(audio, key);
+    }
+    
+    //Checks if the audio is playing and removes the class playing
+    if (isPlaying(audio)) {
+      key.classList.remove('playing');
     }
 
   });
 });
 
-
+//Checks if the audio is playing
 function isPlaying(audio) {
   if (audio && !audio.paused) {
     return true;
   } else {
     return false;
   }
+}
+
+function playAudio(audio, key) {
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
 }
